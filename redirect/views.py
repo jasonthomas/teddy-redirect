a7675b39f0c863fa45628c2eb9fc6a3e10076c73
Jason Thomas <jason@lithiumfox.com>
2012-09-16 21:37:21 -0400

    show recent submitted urls

--- redirect/views.py
+++ redirect/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.conf import settings
 from django.views.decorators.cache import cache_page
+from django.template.response import TemplateResponse
 from redirect.models import Url
 
 @cache_page(60 * 900)
@@ -15,3 +16,9 @@ def get_short(request, short):
 
 def new_short(request):
     return HttpResponse("Short not found in system", content_type="text/plain")
+
+def recent(request, limit=20):
+    try:
+        return TemplateResponse(request, 'recent.html', {'recent_url': Url.objects.all().order_by('-timestamp')[:limit]})
+    except Exception:
+        return HttpResponse("shit is broken", content_type="text/plain")
